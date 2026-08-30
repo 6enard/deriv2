96d02a1608da14d09c9d96f6389948c32d0de261
6enard <109796647+6enard@users.noreply.github.com>
2026-08-31 01:58:58 +0300

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-```tsx
+
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
@@ -1133,4 +1133,4 @@ function Step({
     </div>
   )
 }
-```
+
